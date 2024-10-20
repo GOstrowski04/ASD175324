4337f62f8e0535f50f05a5a60d55a1f72a1cfd34
GOstrowski04 <148046211+GOstrowski04@users.noreply.github.com>
2024-10-20 14:47:01 +0200

    Add files via upload

---
+++ SortowaniaProjekt/175324ASD/175324ASD/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace _175324ASD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(166, 326);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "InsertSort";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(50, 20);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            50000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(68, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(138, 80);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(68, 39);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Generuj";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 293);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Czas sortowania:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(63, 326);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 44);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "BubbleSort";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(63, 238);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(697, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(51, 30);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(215, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(118, 80);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(85, 39);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Konwertuj";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Location = new System.Drawing.Point(63, 30);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(316, 185);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.numericUpDown1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(444, 30);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(316, 185);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(269, 326);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(97, 44);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "MergeSort";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(372, 326);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(97, 44);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "QuickSort";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(475, 326);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(97, 44);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "CountingSort";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ SortowaniaProjekt/175324ASD/175324ASD/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace _175324ASD
+{
+    public partial class Form1 : Form
+    {
+        int[] tab;
+        int randNum;
+        int maxRand = 100;
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.ReadOnly = true;
+        }
+        private bool stringCheck(string input)
+        {
+            
+            var parts = input.Split(',');
+            foreach (var part in parts)
+            {
+                if (!int.TryParse(part.Trim(), out _))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            if (tab.Length > 1)
+            {
+                stopwatch.Start();
+                for (int i = 1; i < tab.Length; i++)
+                {
+                    for (int j = i; j > 0; j--)
+                    {
+                        if (tab[j] < tab[j - 1])
+                        {
+                            int x = tab[j];
+                            tab[j] = tab[j - 1];
+                            tab[j - 1] = x;
+                        }
+                    }
+                }
+                stopwatch.Stop();
+                label1.Text = "Czas sortowania: " + stopwatch.ElapsedMilliseconds + " ms";
+            }
+            textBox1.Text = string.Join(", ", tab);
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            randNum = (int)numericUpDown1.Value;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int[] tempTab = new int[randNum];
+            for (int i = 0; i < randNum; i++)
+            {
+                tempTab[i] = random.Next(0, maxRand);
+            }
+            tab = tempTab;
+            textBox1.Text = string.Join(", ", tab);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            if (tab.Length > 1)
+            {
+                for (int i = tab.Length - 1; i > 0; i--)
+                {
+                    for (int j = 0; j < i; j++)
+                        if (tab[j] > tab[j + 1])
+                        {
+                            int x = tab[j];
+                            tab[j] = tab[j + 1];
+                            tab[j + 1] = x;
+                        }
+                }
+            }
+            stopwatch.Stop();
+            label1.Text = "Czas sortowania: " + stopwatch.ElapsedMilliseconds + " ms";
+            textBox1.Text = string.Join(", ", tab);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (stringCheck(textBox2.Text))
+            {
+                string[] stringArray = textBox2.Text.Split(',');
+                int[] intArray = Array.ConvertAll(stringArray, s => int.Parse(s.Trim()));
+                tab = intArray;
+                textBox1.Text = string.Join(", ", tab);
+            }
+            else
+            {
+                MessageBox.Show("Źle podane dane, proszę o wpisanie liczb według schematu: x, y, z");
+            }
+        }
+        private void MergeSort(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int middle = (left + right) / 2;
+
+                MergeSort(array, left, middle);
+                MergeSort(array, middle + 1, right);
+
+                Merge(array, left, middle, right);
+            }
+        }
+
+        private void Merge(int[] array, int left, int middle, int right)
+        {
+            int n1 = middle - left + 1;
+            int n2 = right - middle;
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+            Array.Copy(array, left, L, 0, n1);
+            Array.Copy(array, middle + 1, R, 0, n2);
+            int i = 0, j = 0;
+            int k = left;
+
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    array[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    array[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+            while (i < n1)
+            {
+                array[k] = L[i];
+                i++;
+                k++;
+            }
+            while (j < n2)
+            {
+                array[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            MergeSort(tab, 0, tab.Length - 1);
+            stopwatch.Stop();
+            label1.Text = "Czas sortowania: " + stopwatch.ElapsedMilliseconds + " ms";
+            textBox1.Text = string.Join(", ", tab);
+        }
+        private void QuickSort(int[] array, int low, int high)
+        {
+            if (low < high)
+            {
+                int pi = Partition(array, low, high);
+                QuickSort(array, low, pi - 1);
+                QuickSort(array, pi + 1, high);
+            }
+        }
+
+        private int Partition(int[] array, int low, int high)
+        {
+            int pivot = array[high];
+            int i = (low - 1);
+
+            for (int j = low; j < high; j++)
+            {
+                if (array[j] <= pivot)
+                {
+                    i++;
+                    Swap(array, i, j);
+                }
+            }
+
+            Swap(array, i + 1, high);
+            return i + 1;
+        }
+
+        private void Swap(int[] array, int i, int j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            QuickSort(tab, 0, tab.Length - 1);
+            stopwatch.Stop();
+            label1.Text = "Czas sortowania: " + stopwatch.ElapsedMilliseconds + " ms";
+            textBox1.Text = string.Join(", ", tab);
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int[] numbers = new int[maxRand - 0 + 1];
+            int[] tempArray = new int[tab.Length];
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < tab.Length; i++)
+            {
+                numbers[tab[i]] += 1;
+            }
+            int j = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int x = numbers[i];
+                for (int z = 0; z < x; z++)
+                {
+                    tempArray[j] = i;
+                    j += 1;
+                }
+            }
+            tab = tempArray;
+            stopwatch.Stop();
+            label1.Text = "Czas sortowania: " + stopwatch.ElapsedMilliseconds + " ms";
+            textBox1.Text = string.Join(", ", tab);
+        }
+
+    }
+}

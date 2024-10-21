ae98f431d15eab00bc550350735589ca1c6be4eb
GOstrowski04 <148046211+GOstrowski04@users.noreply.github.com>
2024-10-21 14:43:14 +0200

    Add files via upload

---
+++ cw04/List.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw04
+{
+    internal class List
+    {
+        public Node head;
+        public Node tail;
+        public int count;
+        public void AddFirst(int liczba)
+        {
+            Node temp = head;
+            head = new Node(liczba);
+            head.next = temp;
+            count += 1;
+        }
+        public void AddLast(int liczba)
+        {
+            Node temp = tail;
+            tail = new Node(liczba);
+            tail.prev = temp;
+            count += 1;
+        }
+        public void RemoveFirst()
+        {
+            head = head.next;
+            count -= 1;
+        }
+        public void RemoveLast()
+        {
+            tail = tail.prev;
+            count -= 1;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+        public Node Get(int index)
+        {
+            Node temp = head;
+            for (int i = 0; i < index; i++)
+            {
+                temp = temp.next;
+            }
+            return temp;
+        }
+    }
+}

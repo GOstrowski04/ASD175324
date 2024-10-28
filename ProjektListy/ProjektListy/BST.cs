a0f6533b619bbdc9c557962b05198397751ee8a1
GOstrowski04 <148046211+GOstrowski04@users.noreply.github.com>
2024-10-28 14:38:01 +0100

    Add files via upload

---
+++ ProjektListy/ProjektListy/BST.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+jeśli liczba jest mniejsza to na lewo, inaczej na prawo
+*/
+namespace ProjektListy
+{
+    public class BST
+    {
+        NodeT root;
+        void Add(int liczba)
+        {
+            if (root == null)
+            {
+                root = new NodeT(liczba);
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
---
+++ ProjektListy/ProjektListy/NodeT.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektListy
+{
+    public class NodeT
+    {
+        NodeT rodzic;
+        NodeT lewe;
+        NodeT prawe;
+        int data;
+        public NodeT(int liczba)
+        {
+            this.data = liczba;
+        }
+    }
+}

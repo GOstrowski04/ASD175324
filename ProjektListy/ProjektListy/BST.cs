d2f9feec4fdfd659e9de359e5b879aeecb88710a
GOstrowski04 <148046211+GOstrowski04@users.noreply.github.com>
2024-10-29 18:36:07 +0100

    Add files via upload

--- ProjektListy/ProjektListy/BST.cs
+++ ProjektListy/ProjektListy/BST.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 /*
 jeśli liczba jest mniejsza to na lewo, inaczej na prawo
 */
@@ -10,8 +11,8 @@ namespace ProjektListy
 {
     public class BST
     {
-        NodeT root;
-        void Add(int liczba)
+        public NodeT root;
+        public void Add(int liczba)
         {
             if (root == null)
             {
@@ -19,7 +20,60 @@ namespace ProjektListy
             }
             else
             {
+                AddRecursive(root, liczba);
+            }
+        }
 
+        private void AddRecursive(NodeT aktualny, int liczba)
+        {
+            if (liczba < aktualny.data)
+            {
+                if (aktualny.lewe == null)
+                {
+                    aktualny.lewe = new NodeT(liczba) { rodzic = aktualny };
+                }
+                else
+                {
+                    AddRecursive(aktualny.lewe, liczba);
+                }
+            }
+            else
+            {
+                if (aktualny.prawe == null)
+                {
+                    aktualny.prawe = new NodeT(liczba) { rodzic = aktualny };
+                }
+                else
+                {
+                    AddRecursive(aktualny.prawe, liczba);
+                }
+            }
+        }
+        public void PopulateTreeView(TreeView treeView)
+        {
+            treeView.Nodes.Clear();
+            if (root != null)
+            {
+                TreeNode rootNode = new TreeNode(root.data.ToString());
+                treeView.Nodes.Add(rootNode);
+                PopulateTreeNode(rootNode, root);
+            }
+        }
+
+        private void PopulateTreeNode(TreeNode treeNode, NodeT node)
+        {
+            if (node.lewe != null)
+            {
+                TreeNode leftNode = new TreeNode(node.lewe.data.ToString());
+                treeNode.Nodes.Add(leftNode);
+                PopulateTreeNode(leftNode, node.lewe);
+            }
+
+            if (node.prawe != null)
+            {
+                TreeNode rightNode = new TreeNode(node.prawe.data.ToString());
+                treeNode.Nodes.Add(rightNode);
+                PopulateTreeNode(rightNode, node.prawe);
             }
         }
     }
